63a519568dddfa288eb1733e4ed38cc233b54743
Shivan Sivakumaran <me@shivan.xyz>
2025-12-13 20:49:16 +1300

    wip

--- src/routes/slides/+page.svelte
+++ src/routes/slides/+page.svelte
@@ -15,38 +15,96 @@
 
 <svelte:head>
 	<title>Progressive Enhancement - Demo</title>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-		rel="stylesheet"
-	/>
 </svelte:head>
 
 <div class="reveal">
 	<div class="slides">
 		<section>
 			<h1>Progressive Enhancement</h1>
-			<p>2025-12-12</p>
+			<p>2025-12-19</p>
+		</section>
+		<section>
+			<h2>!(What?)</h2>
+			<code>Progressive Enhancement != No Javascript</code>
 		</section>
 		<section>
 			<h2>What?</h2>
+			<blockquote>
+				Progressive enhancement is a design philosophy that provides a baseline of essential content
+				and functionality to as many users as possible, while delivering the best possible
+				experience only to users of the most modern browsers that can run all the required code.
+			</blockquote>
+			<a
+				href="https://developer.mozilla.org/en-US/docs/Glossary/Progressive_Enhancement"
+				target="_blank"
+			>
+				MDN Definition of Progressive Enhancemnet
+			</a>
+			<p>Similar to <em>Gradual Degradation</em></p>
+		</section>
+		<section>
+			<h2>Recommended Resources</h2>
+			<ul>
+				<li>
+					<a href="https://www.youtube.com/watch?v=Ji4Y5vo-gOg" target="_blank">
+						Paolo Ricciuti - Progressively enhanced apps with Svelte
+					</a>
+				</li>
+				<li>
+					<a href="https://github.com/sveltejs/svelte/discussions/10085" target="_blank">
+						Svelte's Tenets
+					</a>
+				</li>
+				<li>
+					<a href="https://www.youtube.com/watch?v=UegUi2fWBaU" target="_blank">
+						Rich Harris – North Star, JSNation US 2024
+					</a>
+				</li>
+			</ul>
 		</section>
 	</div>
 </div>
 
 <style>
+	:root {
+		--dim-green: hsla(120 100% 30% / 1);
+		--green: hsla(120 100% 50% / 1);
+		--hl-green: hsla(120 100% 65% / 1);
+		--v-hl-green: hsla(120 100% 85% / 1);
+		--f-v-hl-green: hsla(120 100% 85% / 0.6);
+		--text: hsla(120 100% 50% / 1);
+		--navy: hsla(218 94% 7% / 1);
+
+		--background: var(--navy);
+		--text: hsla(120 40% 95% / 1);
+	}
 	div.reveal {
-		background-color: hsla(120 10% 10% / 1);
-		color: hsla(120 40% 95% / 1);
+		background-color: var(--background);
+		color: var(--text);
 		& h1 {
-			color: hsla(120 100% 50% / 1);
+			color: var(--green);
 			font-size: 3rem;
+			text-shadow: 0rem 0rem 1.25rem var(--f-v-hl-green);
 		}
 		& h2 {
-			color: hsla(120 100% 50% / 1);
-			font-size: 3rem;
+			color: var(--green);
+			font-size: 2.75rem;
 		}
-		font-family: Poppins;
+		a {
+			color: var(--dim-green);
+			transition:
+				color 0.3s ease-in-out,
+				text-shadow 0.3s ease-in-out;
+			&:hover {
+				color: var(--hl-green);
+				text-shadow: 0rem 0rem 0.75rem var(--f-v-hl-green);
+			}
+			&:active {
+				color: var(--v-hl-green);
+			}
+		}
+		ul {
+			text-align: start;
+		}
 	}
 </style>
